8dbd4562ba70482ee5d28c10c758dc39ccbd74d8
sid2417 <siddhartha.java9398@gmail.com>
2025-11-04 17:50:37 +0530

    This is Morrisons Project

--- 1-vpc/outputs.tf
+++ 1-vpc/outputs.tf
@@ -1,17 +1,32 @@
-# output "az_names" {
-#     value = module.morrisons_vpc.az_info
+output "az_names" {
+    value = module.morrisons_vpc.az_info
   
-# }
+}
 
-# output "default_vpc_info" {
-#     value = module.morrisons_vpc.default_vpc
+output "default_vpc_info" {
+    value = module.morrisons_vpc.default_vpc
   
-# }
+}
 
-# output "morrisons_vpc_id" {
-#     value = module.morrisons_vpc.main_vpc_id
+output "morrisons_vpc_id" {
+    value = module.morrisons_vpc.main_vpc_id
   
-# }
+}
+
+output "morrisons_public_subnet_ids" {
+    value = module.morrisons_vpc.public_subnet_ids
+  
+}
+
+output "morrisons_private_subnet_ids" {
+    value = module.morrisons_vpc.private_subnet_ids
+  
+}
+
+output "morrisons_database_subnet_ids" {
+    value = module.morrisons_vpc.database_subnet_ids
+  
+}
 
 
 ### we are storing key-values in ssm-parameter store
--- 1-vpc/ssm-parameter.tf
+++ 1-vpc/ssm-parameter.tf
@@ -3,6 +3,33 @@ resource "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.environment}/vpc-id"
   description = "Stores VPC ID for use in other modules"
   type  = "String"
-  value = module.morrisons_vpc.main_vpc_id  ## here we are refferring from outputs.tf file in morrisons_vpc module 
+  value = module.morrisons_vpc.main_vpc_id  ## here we are refferring from outputs.tf file terraform-vpc-bykithu
   overwrite = true # By default, Terraform doesn’t overwrite existing SSM parameters (for safety)
+}
+
+#["id1","id2"] terraform format
+# id1, id2 -> AWS SSM format
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+  description = "Stores public_subnet_ids for use in other modules"
+  type  = "StringList"
+  value = join(",", module.morrisons_vpc.public_subnet_ids ) # converting list to string list  
+  overwrite = true 
+}
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+  description = "Stores private_subnet_ids for use in other modules"
+  type  = "StringList"
+  value = join(",", module.morrisons_vpc.private_subnet_ids ) # converting list to string list  
+  overwrite = true 
+}
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/database_subnet_ids"
+  description = "Stores database_subnet_ids for use in other modules"
+  type  = "StringList"
+  value = join(",", module.morrisons_vpc.database_subnet_ids ) # converting list to string list  
+  overwrite = true 
 }
